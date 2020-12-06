4ac841ec8780081f9c122b8d25fa4cb9acaba64b
BlasphemyAngels <BlasphemyAngels@gmail.com>
2020-12-06 14:51:43 +0800

    deploy 20201206-14_51_43

---
+++ deploy.sh
@@ -0,0 +1,11 @@
+deploy_dir="/Users/caochenglong/Documents/pro/BlasphemyAngels.github.io"
+cp -rf /Users/caochenglong/Documents/pro/atlantis/_site/* /Users/caochenglong/Documents/pro/BlasphemyAngels.github.io/
+cd ${deploy_dir}
+
+date_str=$(date +%Y%m%d-%H_%M_%S)
+
+git add .
+
+git commit -m "deploy ${date_str}"
+
+git push -u origin main
